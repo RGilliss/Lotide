1bd419e2c966b7d77cb21a4b39f2572b081bdb1d
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-01 22:50:15 +0000

    A better console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌  Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// Test Code
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
