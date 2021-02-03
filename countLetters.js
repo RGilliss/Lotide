5ebfbaa4421ff181ef67df14bd3b03f105140df9
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-03 22:21:43 +0000

    created countLetters function

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(word) {
+  let obj = {};
+  for(const letter of word){
+    if(letter in obj){
+      obj[letter] += 1;
+    } else {
+
+      obj[letter] = 1;
+    }
+  
+  }
+  // console.log(obj);
+  return obj;
+};
+
+const result = countLetters("meateater");
+const result1 = countLetters("lhl");
+assertEqual(result['m'], 1);
+assertEqual(result1['l'], 2);
