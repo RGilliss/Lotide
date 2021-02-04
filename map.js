b2593cfb1229d7e8cfec7a8abba2cbc44deca8fa
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-04 22:08:39 +0000

    Created map higher-order-function and takeUntil function

---
+++ map.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const assertArraysEqual = function(ar1, ar2) {
+
+  if (ar1 === ar2) {
+    
+    return true;
+  } else if (ar1 === null || ar2 === null) {
+    console.log(`❌❌❌  Assertion Failed: ${ar1} !== ${ar2}`);
+    return false;
+  } else if (ar1.length !== ar2.length) {
+    console.log(`❌❌❌  Assertion Failed: ${ar1} !== ${ar2}`);
+    return false;
+  }
+  for (let i = 0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      console.log(`❌❌❌  Assertion Failed: ${ar1} !== ${ar2}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`);
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const fish = ["trout", "Salmon", "", "char"];
+const empty = [];
+
+const map = (myArray, cb) => {
+  const results = [];
+  for (let item of myArray) {
+    
+    results.push(cb(item));
+   
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+assertArraysEqual(map(fish, word => word.length), [5, 6, 0, 4]);
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(empty, word => word[0],), []);
---
+++ takeUntil.js
@@ -0,0 +1,48 @@
+const assertArraysEqual = function(ar1, ar2) {
+
+  if (ar1 === ar2) {
+    
+    return true;
+  } else if (ar1 === null || ar2 === null) {
+    console.log(`❌❌❌  Assertion Failed: ${ar1} !== ${ar2}`);
+    return false;
+  } else if (ar1.length !== ar2.length) {
+    console.log(`❌❌❌  Assertion Failed: ${ar1} !== ${ar2}`);
+    return false;
+  }
+  for (let i = 0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      console.log(`❌❌❌  Assertion Failed: ${ar1} !== ${ar2}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`);
+  return true;
+};
+
+const takeUntil = (array, callback) => {
+  let result = [];
+  let end;
+  for (const item of array) {
+    if (callback(item)) {
+      end = array.indexOf(item);
+    } else {
+      result = array.slice(0, end);
+    }
+  }
+  return result;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
