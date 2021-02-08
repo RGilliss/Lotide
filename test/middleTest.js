53ac364bbca928267a8d7d8f7e9cde34b0fdbaf2
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-08 23:08:45 +0000

    mocha and chai implimented on middleTest function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,21 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
+describe('#middle', () => {
+  it("should return [2, 19] for [1, 5, 2, 19, 8, -1]", () => {
+    assert.deepEqual(middle([1, 5, 2, 19, 8, -1]), [2, 19]);
+  });
+  it("should return [19] for [1, 5, 2, 19, 8, -1, 20]", () => {
+    assert.deepEqual(middle([1, 5, 2, 19, 8, -1, 20]), [19]);
+  });
+  it("should return [] when given an array with two values", () => {
+    assert.deepEqual(middle([2, 3]), []);
+  });
+  it("should return [] when given an array with one value", () => {
+    assert.deepEqual(middle([2]), []);
+  });
+  it("should return [] when given an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
 
-assertArraysEqual(middle([1, 5, 2, 19, 8, -1]), [2, 19]);
-assertArraysEqual(middle([1, 5, 2, 19, 8, -1, 20]), [19]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle(['a', 'b', 'c', 'd']), ['b', 'c']);
