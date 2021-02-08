0fd9168f06b6a099c715e24b9761159d2f8d9b8d
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-08 18:44:58 +0000

    modules exported assertEqual and head to assertEqualTest and headTest

--- assertEqual.js
+++ assertEqual.js
@@ -7,7 +7,5 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// Test Code
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,17 +1,8 @@
-const assertEqual = function(actual, expected) {
- 
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   let first = array[0];
   return first;
 };
 
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello"]), "Hello");
+module.exports = head;
